12bdd21ec920960fb203ce4aeb36402e7e2361f4
Nick Luo <nick.luo@outlook.com>
2020-12-15 21:26:06 +1000

    Initial implementation done

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,41 @@
 const { httpGet } = require('./mock-http-interface');
 
+const SUCCESS_KEY = 'Arnie Quote';
+const ERROR_KEY = 'FAILURE';
+
+/**
+ * 
+ * @param {{ status: string, value: {status: number, body: string}, reason: string }} A resolved promise object 
+ * @returns {{['Arnie Quote' | 'FAILURE']: string}} the transformed object   
+ */
+const handleResponse = (responseObj) => {
+  const { value } = responseObj;
+  const { status, body } = value;
+
+  const messageObj = JSON.parse(body);
+
+  if (status === 200) {
+    return {
+      [SUCCESS_KEY]: messageObj.message
+    };
+  }
+
+  return {
+    [ERROR_KEY]: messageObj.message
+  };
+}
+
+/**
+ * @param {urls string[]} a list of urls
+ * @returns {Promise{ ['Arnie Quote' | 'FAILURE']: string }[]} a result array
+ */
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+
+  const requests = urls.map(url => httpGet(url));
+
+  const responseObjs = await Promise.allSettled(requests);
+
+  return responseObjs.map(handleResponse);
 };
 
 module.exports = {
