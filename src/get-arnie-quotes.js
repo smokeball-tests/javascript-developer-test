35181428a7e9b89c43db671f2e4ce18e94ed9403
Anil Kumar <ozanilkumar18@gmail.com>
2021-09-03 17:30:11 +1000

    feat: arnie quotes api logic

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,18 @@
 const { httpGet } = require('./mock-http-interface');
 
+const SUCCESS_KEY = "Arnie Quote";
+const FAILED_KEY = "FAILURE";
+
+const createHttpPromise = async (url) => {
+  return await httpGet(url).then(({ body, status }) => {
+    const message = JSON.parse(body).message;
+    return status === 200 ? { [SUCCESS_KEY]: message } : { [FAILED_KEY]: message };
+  });
+}
+
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const res = await Promise.all(urls.map(url => createHttpPromise(url)));
+  return res;
 };
 
 module.exports = {
