b41f1886cc1efff8f02e15da1e2f167d82cd55c3
virapyan <virapyan@gmail.com>
2022-02-03 13:42:05 +1100

    feat: [jdt-1]
    Implement getArnieQuotes method.

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,21 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
+
+const parseResponses = (rawResponses) => {
+  return rawResponses.map((resp) => {
+    const resKey = resp.status === 200 ? `Arnie Quote` : `FAILURE`;
+    const body = JSON.parse(resp.body);
+    return { [resKey]: body.message };
+  });
+};
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const rawResp = await Promise.all(
+    urls.map(async (url) => {
+      const response = await httpGet(url);
+      return response;
+    })
+  );
+  return parseResponses(rawResp);
 };
 
 module.exports = {
