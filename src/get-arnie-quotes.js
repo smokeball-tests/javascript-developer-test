44c9ee639688594683ab63b053a4281ad428216c
Joseph Park <joseph.thomas.park@gmail.com>
2022-02-16 12:54:05 +1100

    Implement getArnieQuotes function

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,9 +1,40 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const results = [];
+  const getArnieQuote = async (url) => {
+    const { status, body } = await httpGet(url);
+    const message = JSON.parse(body).message;
+
+    if (status === 200) {
+      pushObjectToArray({
+        array: results,
+        key: 'Arnie Quote',
+        value: message,
+      });
+    } else {
+      pushObjectToArray({
+        array: results,
+        key: 'FAILURE',
+        value: message,
+      });
+    }
+  };
+
+  await Promise.all(
+    urls.map(async (url) => {
+      return await getArnieQuote(url);
+    }),
+  );
+
+  return results;
 };
+
+function pushObjectToArray({ array, key, value }) {
+  array.push({
+    [key]: value,
+  });
+}
 
 module.exports = {
   getArnieQuotes,
