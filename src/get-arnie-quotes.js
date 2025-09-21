0e04088db04e10b6f5b678a8670e219dbcdc3520
Victor Zhang <victorzw895@gmail.com>
2025-09-21 10:09:22 +1000

    feat: adding implementation to getArnieQuotes

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,10 +1,24 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require('./mock-http-interface')
+
+const errorMessage = { FAILURE: 'Unable to retrieve result message' }
+
+const mapResponseToArnieQuote = (result) => {
+	const { status, body } = result?.value ?? {}
+	const parsedBody = body && JSON.parse(body)
+
+	if (!parsedBody || !parsedBody.message) return errorMessage
+
+	const resultKey = status === 200 ? 'Arnie Quote' : 'FAILURE'
+
+	return { [resultKey]: parsedBody.message }
+}
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
-};
+	const results = await Promise.allSettled(urls.map(httpGet))
+
+	return results.map(mapResponseToArnieQuote)
+}
 
 module.exports = {
-  getArnieQuotes,
-};
+	getArnieQuotes,
+}
