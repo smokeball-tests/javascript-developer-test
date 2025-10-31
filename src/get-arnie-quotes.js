e5daba295f9f2b9eb663767ed69c7302034a9dce
PreetPal716 <preetpals716@gmail.com>
2025-10-31 20:56:01 +1100

    added getArnieQuotes func

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,15 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  try {
+    const results = await Promise.all(urls.map(async (url) => {
+      const res = await httpGet(url);
+      return res.status === 200 ? { 'Arnie Quote': (JSON.parse(res.body)).message } : { 'FAILURE': 'Your request has been terminated' };
+    }));
+    return results;
+  } catch (err) {
+    throw new Error('Failed to fetch Arnie quotes');
+  }
 };
 
 module.exports = {
