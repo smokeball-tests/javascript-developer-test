cb4a984d8c5e5df156f3cdffef62e1a56c3bac83
Dhaivat Parikh <dhaivat28@gmail.com>
2025-11-10 18:08:23 +1100

    Solution

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,25 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
+
+const SUCCESS_KEY = "Arnie Quote";
+const FAILURE_KEY = "FAILURE";
+const FAILURE_CODE = 500;
+
+const parseBodyMsg = (body) => {
+  try {
+    return JSON.parse(body).message ?? "";
+  } catch (error) {
+    return "";
+  }
+};
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const responses = await Promise.all(urls.map((url) => httpGet(url)));
+
+  const results = responses.map(({ status, body }) => ({
+    [status === FAILURE_CODE ? FAILURE_KEY : SUCCESS_KEY]: parseBodyMsg(body),
+  }));
+
+  return results;
 };
 
 module.exports = {
