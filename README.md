303a95899155392e8a8cf9ea31b4b381eeb6c096
zax <zaxcool@hotmail.com>
2025-11-20 09:05:56 +1100

    feat: implement get arnie quotes inner logic

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## System Requirements
 
 The coding challenge requires the following to be installed on your development machine;
-* [nodejs](https://nodejs.org/en/download/) version 10 or above.
+* [nodejs](https://nodejs.org/en/download/) version 18.14.0 or above (required for Jest 30).
 * A [git](https://git-scm.com/downloads) client.
 
 ## Installation
--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,20 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  // Execute all HTTP requests in parallel for performance
+  const promises = urls.map(async (url) => {
+    const response = await httpGet(url);
+    const { message } = JSON.parse(response.body);
+    
+    // Return success or failure object based on status code
+    if (response.status === 200) {
+      return { 'Arnie Quote': message };
+    } else {
+      return { 'FAILURE': message };
+    }
+  });
+
+  return Promise.all(promises);
 };
 
 module.exports = {
