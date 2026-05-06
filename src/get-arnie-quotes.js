b51ddd4b168e86602b307a4cf8c651acfd1c8854
Iain Durham <iain_durham@trendmicro.com>
2026-05-06 16:19:03 +1000

    feat: fetch arnie quotes

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,16 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  return Promise.all(urls.map(async (url) => {
+    const { status, body } = await httpGet(url);
+    const { message } = JSON.parse(body);
+
+    if (status !== 200) {
+      return { 'FAILURE': message };
+    }
+
+    return { 'Arnie Quote': message };
+  }));
 };
 
 module.exports = {
