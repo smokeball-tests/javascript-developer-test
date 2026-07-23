5d861ed2c394512dac2dca0e939cd8b32faaec22
mike <itsmikean@gmail.com>
2026-07-23 14:20:55 +1000

    feat: Implement getArnieQuotes function to fetch and parse quotes from provided URLs

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,11 +1,23 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
 
 /**
  * @param {GetArnieQuotesInput} urls
  * @returns {ArnieQuoteResponse}
  */
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
+  const quotes = await Promise.all(
+    // map over the urls and make a request to each url
+    urls.map(async (url) => {
+      const response = await httpGet(url);
+      // parse the response body
+      const { message } = JSON.parse(response.body);
+      // return the response body if the status is 200, otherwise return the failure message
+      return response.status === 200
+        ? { "Arnie Quote": message }
+        : { FAILURE: message };
+    }),
+  );
+  return quotes;
 };
 
 module.exports = {
