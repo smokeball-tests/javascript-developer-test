20538596c2559747af0251915db8b067fe2dbb46
Adityaa Chaubey <adityaa.chaubey@gmail.com>
2026-08-04 11:35:37 +1000

    Implement getArnieQuotes function

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -5,7 +5,17 @@ const { httpGet } = require('./mock-http-interface');
  * @returns {ArnieQuoteResponse}
  */
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
+  // Run all requests in parallel to keep things fast.
+  return Promise.all(
+    urls.map(async (url) => {
+      const response = await httpGet(url);
+      const { message } = JSON.parse(response.body);
+  // Return the message in the correct format based on the response status.
+      return response.status === 200
+        ? { 'Arnie Quote': message }
+        : { 'FAILURE': message };
+    })
+  );
 };
 
 module.exports = {
